bede4c80456d389b4dc8b503ac3be1489f12fab5
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-06-03 19:03:19 -0400

    added styling elements from starter code(I had forgotten about the stater code). Began building variables and functions.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,28 @@
+var userInput;
+var currentTime
+var localStorage = {
+  8: "",
+  9:"",
+  10:""
+
+};
+
 function clock() {
   var currentTime = moment().format("LLL");
   $("#display-date").text(currentTime);
 }
 setInterval(clock, 1000);
+$(".hour");
+$(".input");
+$(".saveBtn").addEventListener(click);
+
+
+
+function updateTask(hour,userInput){
+
+}
+
+function init(){
+//pull from local storage and dynamically populate list
+
+}
