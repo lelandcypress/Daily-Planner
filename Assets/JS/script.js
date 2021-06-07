bc4ef90f47a10d56e6891b06ff5acc2167e62a16
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-06-06 21:11:27 -0400

    app meets ACs, working on MD and refactoring

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   function clock() {
-    var clock = moment().format("LLL");
+    var clock = moment().format("dddd, MMMM Do, LT");
     $("#display-date").text(clock);
   }
   setInterval(clock, 1000);
@@ -23,14 +23,33 @@ $(document).ready(function () {
     }
   }
 
-  $(".saveBtn").click(function () {
-    taskAtEight = $("#08").val();
-    taskAtNine = $("#09").val();
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+
+    var taskAtEight = $("#08").val();
+    var taskAtNine = $("#09").val();
+    var taskAtTen = $("#10").val();
+    var taskAtEleven = $("#11").val();
+    var taskAtNoon = $("#12").val();
+    var taskAtOne = $("#13").val();
+    var taskAtTwo = $("#14").val();
+    var taskAtThree = $("#15").val();
+    var taskAtFour = $("#16").val();
+    var taskAtFive = $("#17").val();
 
     var savedTasks = {
       eightAM: taskAtEight,
       nineAM: taskAtNine,
+      tenAM: taskAtTen,
+      elevenAM: taskAtEleven,
+      noon: taskAtNoon,
+      onePM: taskAtOne,
+      twoPM: taskAtTwo,
+      threePM: taskAtThree,
+      fourPM: taskAtFour,
+      fivePM: taskAtFive,
     };
+
     localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
   });
 
@@ -39,6 +58,15 @@ $(document).ready(function () {
     oldTasks = JSON.parse(retrieve);
     $("#08").val(oldTasks.eightAM);
     $("#09").val(oldTasks.nineAM);
+    $("#10").val(oldTasks.tenAM);
+    $("#11").val(oldTasks.elevenAM);
+    $("#12").val(oldTasks.noon);
+    $("#13").val(oldTasks.onePM);
+    $("#14").val(oldTasks.twoPM);
+    $("#15").val(oldTasks.threePM);
+    $("#16").val(oldTasks.fourPM);
+    $("#17").val(oldTasks.fivePM);
+
     changeColor();
     setInterval(changeColor(), 1000 * 60 * 5);
   }
